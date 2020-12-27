be0f7a38da5fd80a6f40f461c63e1f4e797d38cb
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-27 20:17:21 +0530

    Added Root Query, Books, BookType And AuthorType

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const RootQueryType = new GraphQLObjectType({
     description: "Root Query",
     fields: () => ({
         books: {
-            type: GraphQLList(BookType),
+            type: new GraphQLList(BookType),
             description: "List of all books",
             resolve: () => books
         }
