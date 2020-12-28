37a7a77e2587be867c5ebe5baa22cf0b75f59c3d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-28 10:36:51 +0530

    Added Query For Single Author And Book

--- index.js
+++ index.js
@@ -42,6 +42,26 @@ const RootQueryType = new GraphQLObjectType({
     name: "Query",
     description: "Root Query",
     fields: () => ({
+        book: {
+            type: BookType,
+            description: "A Single Book",
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve: (source, args) => {
+                return books.find(book => book.id === args.id)
+            }
+        },
+        author: {
+            type: AuthorType,
+            description: "A Single Author",
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve: (source, args) => {
+                return authors.find(author => author.id === args.id)
+            }
+        },
         books: {
             type: new GraphQLList(BookType),
             description: "List of all books",
